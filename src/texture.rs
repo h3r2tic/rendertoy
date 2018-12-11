bdf934440883ebe2409336a8c407d2ee4953248b
Tomasz Stachowiak <h3@h3.gd>
2018-12-12 00:08:23 +0100

    Add basic image loading

--- src/texture.rs
+++ src/texture.rs
@@ -7,7 +7,8 @@ use snoozy::*;
 
 snoozy! {
     fn load_tex(ctx: &mut Context, path: &AssetPath) -> Result<Texture> {
-        use image::GenericImageView;
+        use image::{DynamicImage, GenericImageView};
+		use image::imageops::flip_vertical;
 
         let blob = ctx.get(&load_blob(path.clone()))?;
         let img = image::load_from_memory(&blob.contents)?;
@@ -15,7 +16,22 @@ snoozy! {
         let dims = img.dimensions();
         println!("Loaded image: {:?} {:?}", dims, img.color());
 
-        // TODO: upload pixels
-        Ok(backend::texture::create_texture(TextureKey{ width: dims.0, height: dims.1, format: gl::SRGB8_ALPHA8 }))
+        let res = backend::texture::create_texture(TextureKey{ width: dims.0, height: dims.1, format: gl::SRGB8_ALPHA8 });
+
+        unsafe { gl::BindTexture(gl::TEXTURE_2D, res.texture_id); }
+
+        match img {
+            DynamicImage::ImageRgb8(ref img) => unsafe {
+				let img_flipped = flip_vertical(img);
+				gl::TexSubImage2D(gl::TEXTURE_2D, 0, 0, 0, dims.0 as i32, dims.1 as i32, gl::RGB, gl::UNSIGNED_BYTE, std::mem::transmute(img_flipped.into_raw().as_ptr()));
+			},
+            DynamicImage::ImageRgba8(ref img) => unsafe {
+				let img_flipped = flip_vertical(img);
+				gl::TexSubImage2D(gl::TEXTURE_2D, 0, 0, 0, dims.0 as i32, dims.1 as i32, gl::RGBA, gl::UNSIGNED_BYTE, std::mem::transmute(img_flipped.into_raw().as_ptr()));
+			},
+            _ => return Err(format_err!("Unsupported image format"))
+        }
+
+        Ok(res)
     }
 }
