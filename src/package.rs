53433622015ee060f7a433d51a3e7000ff6d155a
Tomasz Stachowiak <h3@h3.gd>
2019-11-13 12:38:09 +0100

    Replace cargo with cargo-metadata

--- src/package.rs
+++ src/package.rs
@@ -1,52 +1,38 @@
-use cargo::core::source::MaybePackage;
-use cargo::core::Workspace;
-use cargo::ops::load_pkg_lockfile;
-use cargo::util::{important_paths, Config};
+use cargo_metadata::MetadataCommand;
 use snoozy::*;
+use std::collections::HashMap;
 
-#[derive(Hash, Debug)]
+#[derive(Debug)]
 pub struct CargoPackageMap {
-    pub name: String,
-    pub path: String,
-    pub deps: Vec<(String, String)>,
+    pub deps: HashMap<String, String>,
 }
 
 #[snoozy]
 pub fn load_cargo_package_map(_ctx: &mut Context) -> Result<CargoPackageMap> {
-    let config = Config::default()?;
-    let package_lock = config.acquire_package_cache_lock()?;
+    let metadata = MetadataCommand::new()
+        .manifest_path("./Cargo.toml")
+        .exec()
+        .unwrap();
 
-    let root = important_paths::find_root_manifest_for_wd(config.cwd())?;
-    println!("Loading cargo package map from {}", root.to_string_lossy());
+    let deps = metadata
+        .packages
+        .iter()
+        .map(|p| {
+            assert_eq!(
+                p.manifest_path.file_name().and_then(|p| p.to_str()),
+                Some("Cargo.toml")
+            );
+            let path = p
+                .manifest_path
+                .parent()
+                .unwrap()
+                .to_string_lossy()
+                .into_owned();
+            (p.name.clone(), path)
+        })
+        .collect();
 
-    let ws = Workspace::new(&root, &config)?;
-    let package = ws.load(&root).unwrap();
-    let resolve = load_pkg_lockfile(&ws)?.unwrap();
-
-    let mut deps = Vec::new();
-
-    //println!("package: {:?}\ndependencies:\n", package.package_id());
-    for (dep, _) in resolve.deps(package.package_id()) {
-        let mut source = dep.source_id().load(&config, &Default::default()).unwrap();
-        source.update()?;
-
-        if let MaybePackage::Ready(pkg) = source.download(dep).unwrap() {
-            //println!("{} -> {}", pkg.name(), pkg.root().to_string_lossy());
-            deps.push((
-                pkg.name().to_string(),
-                pkg.root().to_string_lossy().to_string(),
-            ));
-        }
-    }
-
-    let path = root.parent().unwrap().to_string_lossy().to_string();
-    std::mem::drop(package_lock);
-
-    Ok(CargoPackageMap {
-        name: package.name().to_string(),
-        path,
-        deps,
-    })
+    Ok(CargoPackageMap { deps })
 }
 
 // Explicitly not serializable, so that it doesn't end up auto-cached
@@ -59,15 +45,9 @@ pub fn get_cargo_package_dep_path(
 ) -> Result<CargoDependencyPath> {
     let map = ctx.get(load_cargo_package_map())?;
 
-    if *package == map.name {
-        return Ok(CargoDependencyPath(map.path.clone()));
+    if let Some(path) = map.deps.get(package) {
+        Ok(CargoDependencyPath(path.clone()))
+    } else {
+        Err(format_err!("Package not found: {}", *package))
     }
-
-    for (name, path) in map.deps.iter() {
-        if name == package {
-            return Ok(CargoDependencyPath(path.clone()));
-        }
-    }
-
-    Err(format_err!("Package not found: {}", *package))
 }
